99f0e8877552212a86b61e7e880d928a1915dae0
mohanrajcs03 <97667643+mohanrajcs03@users.noreply.github.com>
2024-11-29 23:01:47 +0530

    CRA & Tailwind Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="font-bold text-center text-3xl">Hello </div>;
+  return <div className="font-bold text-center text-3xl">Hello</div>;
 }
 
 export default App;
